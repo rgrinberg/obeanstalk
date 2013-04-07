b6fef7cdce15486716ef8012322af22c018ac2b1
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-04-07 15:48:40 -0400

    added test data

--- lib_test/test_pure.ml
+++ lib_test/test_pure.ml
@@ -2,8 +2,46 @@ open OUnit
 open Core.Std
 open Beanstalk_cmd
 
-
-let sample_yaml = ""
+let sample_yaml = "
+---
+current-jobs-urgent: 0
+current-jobs-ready: 0
+current-jobs-reserved: 0
+current-jobs-delayed: 0
+current-jobs-buried: 0
+cmd-put: 0
+cmd-peek: 0
+cmd-peek-ready: 0
+cmd-peek-delayed: 0
+cmd-peek-buried: 0
+cmd-reserve: 0
+cmd-reserve-with-timeout: 0
+cmd-delete: 0
+cmd-release: 0
+cmd-use: 0
+cmd-watch: 0
+cmd-ignore: 0
+cmd-bury: 0
+cmd-kick: 0
+cmd-touch: 0
+cmd-stats: 1
+cmd-stats-job: 0
+cmd-stats-tube: 0
+cmd-list-tubes: 0
+cmd-list-tube-used: 0
+cmd-list-tubes-watched: 0
+cmd-pause-tube: 0
+job-timeouts: 0
+total-jobs: 0
+max-job-size: 65535
+current-tubes: 1
+current-connections: 1
+current-producers: 0
+current-workers: 0
+current-waiting: 0
+total-connections: 1
+pid: 13297
+"
 
 let test_yaml () = 
   assert_bool "TODO" true
