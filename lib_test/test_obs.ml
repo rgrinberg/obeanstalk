016cda82ba227120bd52c410469c85a993297ca9
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-04-07 21:40:11 -0400

    added create job test

--- lib_test/test_obs.ml
+++ lib_test/test_obs.ml
@@ -6,8 +6,9 @@ open Async_unix.Async_print
 open Beanstalk_raw
 let (host, port) = ("127.0.0.1", 11300)
 
+let pf = Printf.printf
+
 let verify_beanstalkd_on ~host ~port = 
-  let pf = Printf.printf in 
   let failure () = 
     pf "Beanstalkd instance not running on %s:%d. Aborting\n" host port;
     shutdown 0 in 
@@ -22,14 +23,39 @@ let bsd_running () =
     assert_bool "" true;
   end
 
+let create_job () = 
+  let open Obeanstalk in
+  let open Obeanstalk.Stringly in
+  let job_load = Worker.Job.S.deserialize "sheni" in
+  let bs = connect ~port ~host in 
+  bs >>> begin fun bs ->
+    let open Deferred.Or_error.Monad_infix in 
+    let put_id = ref (-100) in
+    (Worker.put bs ?delay:None ~priority:1 ~ttr:10 ~job:job_load) >>|
+    begin fun job ->
+      assert_equal (Worker.Job.data job) job_load;
+      assert_bool "id exists" ((Worker.Job.id job) > 0);
+      put_id := (Worker.Job.id job);
+      pf "created job\n";
+      return ()
+    end >>= (fun _ ->
+    (Worker.reserve bs ~timeout:2) >>| begin fun job ->
+      assert_equal (Worker.Job.data job) job_load;
+      assert_equal (Worker.Job.id job) (!put_id);
+      pf "reserved job...\n";
+      return ()
+    end) |> ignore
+  end
+
 let test_fixtures =
   "test beanstalk_raw" >:::
     [
       "beanstalkd is running" >:: bsd_running;
+      "create job" >:: create_job
     ]
 
 let _ = run_test_tt ~verbose:true test_fixtures
 
-let _ = (after (sec 1.)) >>> (fun _ -> shutdown 0)
+let _ = (after (sec 2.)) >>> (fun _ -> shutdown 0)
 
 let () = never_returns (Scheduler.go ())
