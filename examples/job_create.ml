114c8af70da76c91d6ceb56f4d21b9de22238a42
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-04-14 00:07:26 -0400

    updated example

--- examples/job_create.ml
+++ examples/job_create.ml
@@ -1,20 +1,23 @@
 open Core.Std
 open Async.Std
+open Async_unix.Async_print
 
 module O = Obeanstalk
 module S = Obeanstalk.Stringly
 
-let pe = Caml.print_endline
-let pf = Caml.Printf.printf
+let pe = print_endline
+let pf = printf
 
 let () = 
   (O.connect ~port:11300 ~host:"127.0.0.1") >>> fun bs -> 
   pe "connected to beanstalkd server";
   S.Worker.put bs ~delay:0 ~priority:2 ~ttr:10 
-  ~job:(S.Worker.Job.S.deserialize "shen sheni") >>> fun job_e ->
-    match job_e with
-    | Result.Ok job -> pf "Created job with id: %d\n" (S.Worker.Job.id job)
-    | Result.Error e -> pe "uh oh"
+    ~job:(S.Worker.Job.S.deserialize "shen sheni") >>> fun job_e ->
+  pe "Received result";
+  match job_e with
+  | Result.Ok job -> pf "Created job with id: %d\n" (S.Worker.Job.id job);
+  | Result.Error e -> pe "uh oh";
+  shutdown 0
 
 let () = never_returns (Scheduler.go ())
 
