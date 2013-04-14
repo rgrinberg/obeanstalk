11ad809b10d16e0c1ae63e1e05e075c980a2ce9f
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-04-14 01:13:25 -0400

    added read job in example

--- examples/job_create.ml
+++ examples/job_create.ml
@@ -8,15 +8,28 @@ module S = Obeanstalk.Stringly
 let pe = print_endline
 let pf = printf
 
+
+let bs = O.connect ~port:11300 ~host:"127.0.0.1"
+
 let () = 
-  (O.connect ~port:11300 ~host:"127.0.0.1") >>> fun bs -> 
+  bs >>> fun bs ->
+  S.Worker.reserve bs >>> (fun job -> 
+    match job with
+    | Result.Ok job ->
+      let data = job |> S.Worker.Job.data |> S.Worker.Job.S.serialize in
+      pf "Received job (%d): %s" (S.Worker.Job.id job) data
+    | Result.Error _ -> pe "uh oh")
+
+let () = 
+  bs >>> fun bs -> 
   pe "connected to beanstalkd server";
   S.Worker.put bs ~delay:0 ~priority:2 ~ttr:10 
     ~job:(S.Worker.Job.S.deserialize "shen sheni") >>> fun job_e ->
   pe "Received result";
-  match job_e with
-  | Result.Ok job -> pf "Created job with id: %d\n" (S.Worker.Job.id job);
-  | Result.Error e -> pe "uh oh";
+  begin match job_e with
+    | Result.Ok job -> pf "Created job with id: %d\n" (S.Worker.Job.id job)
+    | Result.Error e -> pe "uh oh"
+  end;
   shutdown 0
 
 let () = never_returns (Scheduler.go ())
