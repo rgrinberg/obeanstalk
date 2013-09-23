d80b33a1c27d2588c377b96770f24b545e8807e2
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-23 19:49:04 -0400

    added function to yaml mli for testing

--- lib/yaml.mli
+++ lib/yaml.mli
@@ -5,3 +5,7 @@ val to_dict : string -> (string * string) list
 
 (** parse a yaml list of strings *)
 val to_list : string -> string list
+
+(** ONLY FOR TESTS *)
+val split_exn : string -> on:char -> string * string
+
