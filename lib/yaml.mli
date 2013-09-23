1a9ee4300ca41f10b7e08d40caee096b49eaebcb
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-23 19:41:01 -0400

    yaml parsing docs

--- lib/yaml.mli
+++ lib/yaml.mli
@@ -1,4 +1,7 @@
+(** YAML parsing functions for parsing beanstalkd responses *)
 
+(** parse a dictionary. assume all values are strings *)
 val to_dict : string -> (string * string) list
 
+(** parse a yaml list of strings *)
 val to_list : string -> string list
