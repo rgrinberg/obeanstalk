eddcfa8da095e19f189d9bbb98171ec36561e105
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-23 23:29:08 -0400

    changed Or_error.t to Deferred.t

--- lib/beanstalk.mli
+++ lib/beanstalk.mli
@@ -18,18 +18,17 @@ module type Serializable = sig
 end
 
 module Tube : sig
-  open Deferred (* for the correct result type *)
   (** common operations *)
-  val all : conn -> string list Or_error.t
-  val stats : conn -> tube:string  -> (string * string) list Or_error.t
-  val pause : conn -> tube:string -> delay:int -> unit Or_error.t
+  val all : conn -> string list Deferred.t
+  val stats : conn -> tube:string  -> (string * string) list Deferred.t
+  val pause : conn -> tube:string -> delay:int -> unit Deferred.t
   (** consumers *)
-  val watched : conn -> string list Or_error.t
-  val watch : conn -> tube:string -> [`Watching of int] Or_error.t
-  val ignore_tube : conn -> tube:string -> unit Or_error.t
+  val watched : conn -> string list Deferred.t
+  val watch : conn -> tube:string -> [`Watching of int] Deferred.t
+  val ignore_tube : conn -> tube:string -> unit Deferred.t
   (** producers *)
-  val use : conn -> tube:string -> unit Or_error.t
-  val using : conn -> [`Tube of string] Or_error.t
+  val use : conn -> tube:string -> unit Deferred.t
+  val using : conn -> [`Tube of string] Deferred.t
 end
 
 module type Job_intf = sig
@@ -41,28 +40,27 @@ end
 
 module Worker : functor (S : Serializable) -> sig
   module Job : Job_intf
-  open Deferred (* for the correct result type *)
   type s = S.t
   type t = s Job.t
   (** reserving jobs *)
-  val reserve : ?timeout:int -> conn -> t Or_error.t
+  val reserve : ?timeout:int -> conn -> t Deferred.t
   (** job operations *)
   val put : conn -> ?delay:int -> priority:int -> ttr:int -> 
-                    job:S.t -> t Or_error.t
+                    job:S.t -> t Deferred.t
 
-  val bury : conn -> id:int -> priority:int -> unit Or_error.t
-  val delete : conn -> id:int -> unit Or_error.t
-  val touch : conn -> id:int -> unit Or_error.t
-  val release : conn -> id:int -> priority:int -> delay:int ->  unit Or_error.t
+  val bury : conn -> id:int -> priority:int -> unit Deferred.t
+  val delete : conn -> id:int -> unit Deferred.t
+  val touch : conn -> id:int -> unit Deferred.t
+  val release : conn -> id:int -> priority:int -> delay:int ->  unit Deferred.t
   (** peeks *)
-  val peek : conn -> id:int -> t Or_error.t
-  val peek_ready : conn -> t Or_error.t
-  val peek_delayed : conn -> t Or_error.t
-  val peek_buried : conn -> t Or_error.t
+  val peek : conn -> id:int -> t Deferred.t
+  val peek_ready : conn -> t Deferred.t
+  val peek_delayed : conn -> t Deferred.t
+  val peek_buried : conn -> t Deferred.t
   (** kicks *)
-  val kick_bound : conn -> bound:int -> [ `Kicked of int ] Or_error.t
-  val kick_job : conn -> id:int -> unit Or_error.t
+  val kick_bound : conn -> bound:int -> [ `Kicked of int ] Deferred.t
+  val kick_job : conn -> id:int -> unit Deferred.t
 
-  val stats : conn -> id:int -> (string * string) list Or_error.t
+  val stats : conn -> id:int -> (string * string) list Deferred.t
 end
 
