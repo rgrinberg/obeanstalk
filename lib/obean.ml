2392d1e6afe51f0ef076e269d0c0051bac95cfb9
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-23 21:03:21 -0400

    refactored command line parsing

--- lib/obean.ml
+++ lib/obean.ml
@@ -5,22 +5,26 @@ open Async_unix.Async_print
 let readme () = "Example:
 obean blah blah"
 
-let command =
-  Command.async_basic ~summary:"Test client for beanstalkd" ~readme
+let conn () = Obeanstalk.connect ~port:11300 ~host:"127.0.0.1"
+
+let tube =
+  Command.async_basic ~summary:"Inspect tubes"
     Command.Spec.(
       empty
-      +> flag "-tl" (no_arg) ~doc:"List tubes"
-      +> flag "-c" (no_arg) ~doc:"List consumers"
-    ) (fun tubes consumers () -> 
-        Obeanstalk.connect ~port:11300 ~host:"127.0.0.1" >>=
-        (fun bs -> Obeanstalk.Tube.all bs) >>= begin fun res ->
-          begin match res with
-            | Result.Ok s -> begin 
-                print_endline "tubes:"; 
-                List.iter s ~f:print_endline
-              end
-            | Result.Error _ -> print_endline "uh oh" end;
-          return ()
-        end)
+    ) (fun () -> 
+      conn () >>= (fun bs -> Obeanstalk.Tube.all bs)
+      >>= begin fun res ->
+        begin match res with
+          | Result.Ok s -> begin 
+              print_endline "tubes:"; 
+              List.iter s ~f:print_endline
+            end
+          | Result.Error _ -> print_endline "uh oh" end;
+        return ()
+      end)
+
+let command =
+  Command.group ~summary:"beanstalkd command line tool"
+  ["tube", tube]
 
 let () = Exn.handle_uncaught ~exit:true (fun () -> Command.run command)
