d22990c30700f14070e338163e24f9bc5787af73
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-09-30 22:31:47 -0400

    cleaned up job_create

--- examples/job_create.ml
+++ examples/job_create.ml
@@ -2,31 +2,25 @@ open Core.Std
 open Async.Std
 open Async_unix.Async_print
 
-module O = Beanstalk
-module S = O
-
-let pe = print_endline
-let pf = printf
-
-let bs = O.connect ~port:11300 ~host:"127.0.0.1"
+let bs = Beanstalk.connect ~port:11300 ~host:"127.0.0.1"
 
 let job_ready = Ivar.create ()
 
 let () = 
   Ivar.read job_ready >>> fun bs ->
-  S.Worker.reserve bs >>> begin fun job -> 
-    let data = O.Job.data job in
-    pf "Received job (%d): '%s'" (O.Job.id job) data;
+  Beanstalk.Worker.reserve bs >>> begin fun job -> 
+    let data = Beanstalk.Job.data job in
+    printf "Received job (%d): '%s'" (Beanstalk.Job.id job) data;
     upon (after (sec 0.2)) (fun _ -> shutdown 0)
   end
 
 let () = 
   bs >>> fun bs -> 
-  pe "connected to beanstalkd server";
-  (S.Worker.put bs ~delay:0 ~priority:2 ~ttr:10 
+  print_endline "connected to beanstalkd server";
+  (Beanstalk.Worker.put bs ~delay:0 ~priority:2 ~ttr:10 
      ~data:"shen sheni") >>> (fun job_e ->
-      pe "Received result";
-      pf "Created job with id: %d\n" (O.Job.id job_e);
+      print_endline "Received result";
+      printf "Created job with id: %d\n" (Beanstalk.Job.id job_e);
       Ivar.fill job_ready bs)
 
 let () = never_returns (Scheduler.go ())
