1fb53df9937fce26d5c4c9020edcb28d0ab2c103
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-10-04 19:50:58 -0400

    docs

--- lib/beanstalk.mli
+++ lib/beanstalk.mli
@@ -4,36 +4,55 @@ open Core.Std
 open Async.Std (* only dependency is Deferred.t *)
 
 type conn
+(** Beanstalkd connection *)
 
 val connect : host:string -> port:int -> conn Deferred.t
+(** [connect ~host ~port] returns a Deferred that is resolved when the
+    connection * is opened*)
 
 val default_connection : ?host:string -> ?port:int -> unit -> conn Deferred.t
+(** [default_connection ()] creates a connection with the default host and
+ * port. Port 11300 and localhost*)
 
 val quit : conn -> unit Deferred.t
+(** [quit conn] Returns a deferred that is resolved when a connection is closed
+ * *)
 
+(** Representation of beanstalkd jobs *)
 module Job : sig
   type t = private {
     id : int;
     data : string;
   } with sexp
+  (** a representation of a beanstalkd job *)
   val id : t -> int
+  (** [id j] return the id of job [j]*)
   val data : t -> string
+  (* [data j return the raw payload of job [j] *)
 end
 
+(** A module to manipulat beanstalkd tubes *)
 module Tube : sig
   (** common operations *)
   val all : conn -> string list Deferred.t
+  (** [all conn] returns the names of all tubes available *)
   val stats : conn -> tube:string -> (string * string) list Deferred.t
+  (** [stats conn ~tube] return a list of statistics for [tube] *)
   val pause : conn -> tube:string -> delay:int -> unit Deferred.t
   (** consumers *)
   val watched : conn -> string list Deferred.t
+  (** [watched conn] returns a list of all watched tubes *)
   val watch : conn -> tube:string -> [ `Watching of int ] Deferred.t
+  (** [watch conn ~tube] Adds [tube] to the watch list and returns
+   * the number of watched tubes *)
   val ignore_tube : conn -> tube:string -> unit Deferred.t
+  (** [ignore_tube conn ~tube] remove [tube] from the watch list *)
   (** producers *)
   val use : conn -> tube:string -> unit Deferred.t
   val using : conn -> [ `Tube of string ] Deferred.t
 end
 
+(** Job operations within beanstalkd *)
 module Worker : sig
   val reserve : ?timeout:int -> conn -> Job.t Deferred.t
   (** job operations *)
