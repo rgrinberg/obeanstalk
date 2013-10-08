d18389d523df1d5d542a156592ab430c5fc550ab
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-10-07 22:00:43 -0400

    documented some exceptions

--- lib/beanstalk.mli
+++ lib/beanstalk.mli
@@ -87,15 +87,13 @@ module Worker : sig
   val stats : conn -> id:int -> (string * string) list Deferred.t
 end
 
-exception Unexpected_response of string with sexp
 exception Timeout                       with sexp
 exception Out_of_memory                 with sexp
 exception Internal_error                with sexp
 exception Draining                      with sexp
 exception Bad_format                    with sexp
 exception Unknown_command               with sexp
-exception Buried of (int option)        with sexp
-exception Expected_crlf                 with sexp
+exception Buried of int option          with sexp
 exception Job_too_big                   with sexp
 exception Deadline_soon                 with sexp
 exception Not_ignored                   with sexp
@@ -103,3 +101,10 @@ exception Not_connected                 with sexp
 exception Invalid_tube_name             with sexp
 exception Job_not_reserved              with sexp
 exception Beanstalk_not_found           with sexp
+(** Raised when a job is not found. Purposely named not to be confused
+    with [Not_found] from pervaisves *)
+
+exception Unexpected_response of string with sexp
+(** If you ever see this exception. Please contact the library author *)
+exception Expected_crlf                 with sexp
+(** If you ever see this exception. Please contact the library author *)
